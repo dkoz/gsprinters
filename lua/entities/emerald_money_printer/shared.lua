d994e8a409d557bf9989ddb1a8c5f2cffb5bb67e
KoZ <mybbkoz@gmail.com>
2013-08-04 15:58:13 -0400

    added proper credit to original author

--- lua/entities/emerald_money_printer/shared.lua
+++ lua/entities/emerald_money_printer/shared.lua
@@ -1,7 +1,7 @@
 ENT.Type = "anim"
 ENT.Base = "base_gmodentity"
 ENT.PrintName = "Emerald Printer"
-ENT.Author = "KoZ & !!!WARLOCK!!!"
+ENT.Author = "KoZ & TCWilliamson"
 ENT.Spawnable = false
 ENT.AdminSpawnable = false
 
