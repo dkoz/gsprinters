08612152f214cc7195bffa2709c68db345fdfcdf
Koz <drunkenkoz@gmail.com>
2014-12-11 13:17:05 -0500

    Revert "Updated Gemstone Printers"
    This reverts commit 0bb0cb3fd2cea5d35acd5ef6723d65a2bd30bb5d.

---
+++ lua/entities/emerald_money_printer/cl_init.lua
@@ -0,0 +1,33 @@
+include("shared.lua")
+
+function ENT:Initialize()
+end
+
+function ENT:Draw()
+	self:DrawModel()
+	
+	local Pos = self:GetPos()
+	local Ang = self:GetAngles()
+	
+    local owner = self:Getowning_ent()
+	owner = (IsValid(owner) and owner:Nick()) or DarkRP.getPhrase("unknown")
+	
+	local txt1 = gemstone.emeraldname
+	txt2 = "$" ..self:GetNWInt("PrintA")
+	
+	surface.SetFont("HUDNumber5")
+	local TextWidth = surface.GetTextSize(txt1)
+	local TextWidth2 = surface.GetTextSize(txt2)
+	local TextWidth3 = surface.GetTextSize(owner)
+	
+	Ang:RotateAroundAxis(Ang:Up(), 90)
+	
+	cam.Start3D2D(Pos + Ang:Up() * 11.5, Ang, 0.11)
+	    draw.WordBox(2, -TextWidth3*0.5, -78, owner, "HUDNumber5", Color(0, 0, 0, 100), Color(255,255,255,255))
+		draw.WordBox(2, -TextWidth*0.5, -30, txt1, "HUDNumber5", Color(0, 0, 0, 100), Color(255,255,255,255))
+		draw.WordBox(2, -TextWidth2*0.5, 18, txt2, "HUDNumber5", Color(0, 0, 0, 100), Color(255,255,255,255))
+	cam.End3D2D()
+end
+
+function ENT:Think()
+end
---
+++ lua/entities/sapphire_money_printer/init.lua
@@ -0,0 +1,112 @@
+--[[
+Gemstone Printer created by KoZ
+https://github.com/dkoz/gsprinters
+]]--
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+include("shared.lua")
+
+ENT.SeizeReward = 950
+
+local PrintMore
+function ENT:Initialize()
+	local scolor = gemstone.sapphirecolor
+
+	self:SetModel("models/props_c17/consolebox01a.mdl")
+	self:PhysicsInit(SOLID_VPHYSICS)
+	self:SetMoveType(MOVETYPE_VPHYSICS)
+	self:SetSolid(SOLID_VPHYSICS)
+	self:SetColor( scolor )
+	local phys = self:GetPhysicsObject()
+	if phys:IsValid() then phys:Wake() end
+	self.damage = 100
+	self.IsMoneyPrinter = true
+	timer.Simple(1.0, function() PrintMore(self) end)
+	self:SetNWInt("PrintA",0)
+	
+	self.sound = CreateSound(self, Sound("ambient/levels/labs/equipment_printer_loop1.wav"))
+	self.sound:SetSoundLevel(52)
+	self.sound:PlayEx(1, 100)
+end
+
+function ENT:OnTakeDamage(dmg)
+	if self.burningup then return end
+
+	self.damage = (self.damage or 100) - dmg:GetDamage()
+	if self.damage <= 0 then
+		local rnd = math.random(1, 10)
+		if rnd < 6 then
+			self:BurstIntoFlames()
+		else
+			self:Destruct()
+			self:Remove()
+		end
+	end
+end
+
+function ENT:Destruct()
+	local vPoint = self:GetPos()
+	local effectdata = EffectData()
+	effectdata:SetStart(vPoint)
+	effectdata:SetOrigin(vPoint)
+	effectdata:SetScale(1)
+	util.Effect("Explosion", effectdata)
+	DarkRP.notify(self:Getowning_ent(), 1, 4, "Your money printer has exploded!")
+end
+
+function ENT:BurstIntoFlames()
+	DarkRP.notify(self:Getowning_ent(), 1, 4, "Your money printer is overheating!")
+	self.burningup = true
+	local burntime = math.random(8, 18)
+	self:Ignite(burntime, 0)
+	timer.Simple(burntime, function() self:Fireball() end)
+end
+
+function ENT:Fireball()
+	if not self:IsOnFire() then self.burningup = false return end
+	local dist = math.random(5, 50) -- Explosion radius
+	self:Destruct()
+	for k, v in pairs(ents.FindInSphere(self:GetPos(), dist)) do
+		if not v:IsPlayer() and not v:IsWeapon() and v:GetClass() ~= "predicted_viewmodel" and not v.IsMoneyPrinter then
+			v:Ignite(math.random(5, 22), 0)
+		elseif v:IsPlayer() then
+			local distance = v:GetPos():Distance(self:GetPos())
+			v:TakeDamage(distance / dist * 100, self, self)
+		end
+	end
+	self:Remove()
+end
+
+PrintMore = function(ent)
+	if IsValid(ent) then
+		ent.sparking = true
+		timer.Simple(1.0, function() ent:CreateMoneybag() end)
+	end
+end
+
+function ENT:CreateMoneybag()
+	if not IsValid(self) then return end
+	if self:IsOnFire() then return end
+	local MoneyPos = self:GetPos()
+	local printamount = gemstone.sapphireprintamount
+	local printtime = gemstone.sapphireprinttime
+	if math.random(1, 1000) == 3 then self:BurstIntoFlames() end
+	local amount = self:GetNWInt("PrintA") + printamount
+	self:SetNWInt("PrintA",amount)
+	self.sparking = false
+	timer.Simple(printtime, function() PrintMore(self) end)
+end
+
+function ENT:Use(activator)
+	if(activator:IsPlayer()) and self:GetNWInt("PrintA") >= 1 then
+	activator:addMoney(self:GetNWInt("PrintA"));
+	DarkRP.notify(activator, 1, 4, "You have collected $"..self:GetNWInt("PrintA").." from a Sapphire Printer.")
+	self:SetNWInt("PrintA",0)
+	end
+end
+
+function ENT:OnRemove()
+	if self.sound then
+		self.sound:Stop()
+	end
+end
